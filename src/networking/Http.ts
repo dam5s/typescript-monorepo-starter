1e934182482c9fad850d6fe3eb4ea4663acf885c
Damien Le Berrigaud <git@itsdamo.com>
2021-08-24 17:09:15 -0600

    Cleaner http layer

--- src/networking/Http.ts
+++ src/networking/Http.ts
@@ -1,6 +1,7 @@
 import {Decoder} from 'schemawax';
 import {asyncResult, AsyncResult} from '../prelude/AsyncResult';
 import {maybe} from '../prelude/Maybe';
+import {match} from 'ts-pattern';
 
 export type HttpError =
     | { type: 'connection error' }
@@ -15,10 +16,10 @@ export type HttpRequest =
 export type HttpResult<T> = AsyncResult<T, HttpError>
 
 const connectionError: HttpError = {type: 'connection error'};
-const unknownError = (response: Response): HttpError => ({type: 'unknown error', response: response});
-const apiError = (response: Response): HttpError => ({type: 'api error', response: response});
-const serverError = (response: Response): HttpError => ({type: 'server error', response: response});
-const deserializationError = (response: Response): HttpError => ({type: 'deserialization error', response: response});
+const unknownError = (response: Response): HttpError => ({type: 'unknown error', response});
+const apiError = (response: Response): HttpError => ({type: 'api error', response});
+const serverError = (response: Response): HttpError => ({type: 'server error', response});
+const deserializationError = (response: Response): HttpError => ({type: 'deserialization error', response});
 
 const requestInit = (request: HttpRequest): RequestInit =>
     ({method: request.method});
@@ -27,18 +28,13 @@ const sendRequest = (request: HttpRequest): HttpResult<Response> =>
     asyncResult
         .ofPromise(fetch(request.url, requestInit(request)))
         .mapErr((): HttpError => connectionError)
-        .flatMapOk((response: Response) => {
-            switch (response.status) {
-                case 200:
-                    return asyncResult.ok(response);
-                case 400:
-                    return asyncResult.err(apiError(response));
-                case 500:
-                    return asyncResult.err(serverError(response));
-                default:
-                    return asyncResult.err(unknownError(response));
-            }
-        });
+        .flatMapOk((response: Response) =>
+            match(response.status)
+                .with(200, () => asyncResult.ok<Response, HttpError>(response))
+                .with(400, () => asyncResult.err<Response, HttpError>(apiError(response)))
+                .with(500, () => asyncResult.err<Response, HttpError>(serverError(response)))
+                .otherwise(() => asyncResult.err(unknownError(response)))
+        );
 
 const decodeJson = <T>(decoder: Decoder<T>) => (response: Response): HttpResult<T> =>
     asyncResult
