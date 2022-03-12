cf5a19fbec32dad4730fbde40e716438c0fe5061
Damien Le Berrigaud <damien@initialcapacity.io>
2022-03-11 22:32:58 -0700

    Added tests to TypedApi

--- backend/src/ApiSupport/Decoders.ts
+++ backend/src/ApiSupport/Decoders.ts
@@ -1,11 +1,12 @@
 import * as schema from 'schemawax';
+import {DecoderError} from 'schemawax';
 
 const stringToInt: schema.Decoder<number> =
     schema.string.andThen(value => {
         const maybeInt = parseInt(value);
 
         if (isNaN(maybeInt)) {
-            throw new Error('Failed to convert string to int');
+            throw new DecoderError('Failed to convert string to int');
         }
 
         return maybeInt;
---
+++ backend/src/ApiSupport/__tests__/TypedApi.test.ts
@@ -0,0 +1,87 @@
+import {testServer} from '../../TestSupport/TestServer';
+import {typedApi} from '../TypedApi';
+import * as schema from 'schemawax';
+import {decoders} from '../Decoders';
+
+describe('TypedApi', () => {
+
+    const server = testServer.create([
+        typedApi.route({
+            method: 'POST',
+            path: '/tenant/{tenantId}/session',
+            decoders: {
+                body: schema.object({
+                    required: {
+                        username: schema.string,
+                        password: schema.string,
+                    },
+                }),
+                query: schema.object({
+                    required: {
+                        redirect: schema.string,
+                    },
+                }),
+                path: schema.object({
+                    required: {
+                        tenantId: decoders.stringToInt,
+                    },
+                }),
+            },
+            handler: ({body, query, path}, {h}) =>
+                h.response({
+                    username: body.username,
+                    password: body.password,
+                    redirect: query.redirect,
+                    tenantId: path.tenantId,
+                }),
+        }),
+    ]);
+
+    const validBody = {username: 'bob', password: 'secret'};
+
+    test('with valid body, query and path params', async () => {
+        const response = await server.inject({
+            method: 'POST',
+            url: 'http://localhost:3000/tenant/210/session?redirect=example.com',
+            payload: validBody,
+        });
+
+        expect(response.statusCode).toEqual(200);
+        expect(JSON.parse(response.payload)).toEqual({
+            username: 'bob',
+            password: 'secret',
+            redirect: 'example.com',
+            tenantId: 210,
+        });
+    });
+
+    test('with invalid body', async () => {
+        const response = await server.inject({
+            method: 'POST',
+            url: 'http://localhost:3000/tenant/210/session?redirect=example.com',
+            payload: {hello: 'world'},
+        });
+
+        expect(response.statusCode).toEqual(400);
+    });
+
+    test('with invalid query', async () => {
+        const response = await server.inject({
+            method: 'POST',
+            url: 'http://localhost:3000/tenant/210/session',
+            payload: validBody,
+        });
+
+        expect(response.statusCode).toEqual(400);
+    });
+
+    test('with invalid path', async () => {
+        const response = await server.inject({
+            method: 'POST',
+            url: 'http://localhost:3000/tenant/ABCD/session',
+            payload: validBody,
+        });
+
+        expect(response.statusCode).toEqual(400);
+    });
+});
