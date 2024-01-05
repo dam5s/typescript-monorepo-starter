a2226f7a488df099085f7489b63eea57a1001fb7
Damien Le Berrigaud <git@itsdamo.com>
2024-01-05 09:16:10 -0700

    Document jake tasks

--- database/Jakefile.js
+++ database/Jakefile.js
@@ -1,4 +1,4 @@
-const {fail, task, namespace} = require('jake');
+const {fail, task, namespace, desc} = require('jake');
 const {execOrExit} = require('../build-support/ExecOrExit');
 const path = require('path');
 
@@ -13,17 +13,24 @@ const srcFile = (name) => path.join(srcDir, name)
 
 task('fromProjectDir', () => process.chdir(projectDir));
 
-task('create', async () => {
-    await execOrExit(`psql postgres -f ${srcFile('create_local_databases.sql')}`)
-});
+desc('database - install dependencies')
+task('install', ['fromProjectDir'], () => execOrExit('npm install'));
+
+desc('database - create local databases')
+task('create', () => execOrExit(`psql postgres -f ${srcFile('create_local_databases.sql')}`));
 
 namespace('migrate', () => {
+    desc('database - migrate tests database')
     task('tests', ['fromProjectDir'], () => execOrExit('npx db-migrate up -e tests'));
+
+    desc('database - migrate dev database')
     task('dev', ['fromProjectDir'], () => execOrExit('npx db-migrate up -e dev'));
 });
 
-task('migrate', ['migrate:tests', 'migrate:dev']);
+desc('database - migrate local databases')
+task('migrate', ['install', 'migrate:tests', 'migrate:dev']);
 
+desc('database - create new migration')
 task('new-migration', ['fromProjectDir'], async (name) => {
     if (name === undefined) {
         fail('Expected name of migration passed as argument');
